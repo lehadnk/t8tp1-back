4f19691910cdc5260443127f7d38ed02c605554d
Alexey Zauzin <lehadnk@gmail.com>
2024-04-13 20:22:45 +0300

    Changed search optimisation algorithm to step scan

--- src/calculation/calculation.py
+++ src/calculation/calculation.py
@@ -1,13 +1,20 @@
 from datetime import datetime
 
-from calculation.pattern_search import find_maximum_md
+from calculation.step_scan import step_scan
 from dto.schemas import CoefficientSetup, CalculationResult
 
 
 def calculate(cs: CoefficientSetup) -> CalculationResult:
-    func = lambda t1, t2 : cs.a * (cs.g * cs.mu * ((t2 - t1) ** cs.n + (cs.beta * cs.a - t1) ** cs.n))
-    max_t1, max_t2 = find_maximum_md(func, 2, [0, 0], [-3, -2], [3, 6], [0.1, 0.1], 0.001)
-    max_value = func(max_t1, max_t2)
+    f = lambda t1, t2 : cs.a * (cs.g * cs.mu * ((t2 - t1) ** cs.n + (cs.beta * cs.a - t1) ** cs.n))
+
+    limits = [
+        lambda t1, t2: -3 <= t1 <= 3,
+        lambda t1, t2: -2 <= t2 <= 6,
+        lambda t1, t2: t1 - t2 >= -3
+    ]
+
+    max_t1, max_t2 = step_scan(f, limits, [0, 0], [-0.5, 0.5], 0.00001)
+    max_value = f(max_t1, max_t2)
 
     calculation_result = CalculationResult(**(cs.__dict__ | {"t1": max_t1, "t2": max_t2, "s": max_value, "calculated_at": datetime.now()}))
     calculation_result.id = None
